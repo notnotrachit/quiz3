ac93df03f1618cf61801a4482d2762abd4271c4d
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2024-06-23 06:42:12 +0000

    save questions function and updated next config

---
+++ fleek_functions/save_question.js
@@ -0,0 +1,13 @@
+// var jwt = require("jsonwebtoken");
+import { Buffer } from "node:buffer";
+import verify from "./node_modules/jsonwebtoken/index.js";
+const b64publicKey = "";
+const publicKey = Buffer.from(b64publicKey, "base64").toString("utf-8");
+
+export const main = async (params) => {
+  const { body } = params;
+  const decodedToken = verify(body.token, publicKey);
+  return decodedToken;
+};
+
+// main({ method: "GET", path: "/question" }).then(console.log);
