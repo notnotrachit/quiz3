334cacd958462b7e47e966048e76a76d3974c998
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2024-06-29 00:41:45 +0530

    fleek function user quizes and leaderboard

--- fleek_functions/quiz3.js
+++ fleek_functions/quiz3.js
@@ -99,7 +99,7 @@ async function get_quiz(body) {
   return quiz;
 }
 
-async function submit_attempt(body){
+async function submit_attempt(body) {
   const quiz_id = body.quiz_id;
   const user_address = body.user_address;
   const answers = body.answers;
@@ -132,7 +132,7 @@ async function submit_attempt(body){
   const max_score = quiz_data.length;
   for (let i = 0; i < quiz_data.length; i++) {
     console.log(quiz_data[i].answer, answers[i]);
-    if(quiz_data[i].answer === answers[i]){
+    if (quiz_data[i].answer === answers[i]) {
       score += 1;
     }
   }
@@ -158,7 +158,9 @@ async function submit_attempt(body){
       {
         type: "execute",
         stmt: {
-          sql: `INSERT INTO attempts (id, quiz_id, user_address, answers, score, max_score) VALUES ('${attempt_id}' ,'${quiz_id}', '${user_address}', '${JSON.stringify(answers)}', ${score}, ${max_score})`,
+          sql: `INSERT INTO attempts (id, quiz_id, user_address, answers, score, max_score) VALUES ('${attempt_id}' ,'${quiz_id}', '${user_address}', '${JSON.stringify(
+            answers
+          )}', ${score}, ${max_score})`,
         },
       },
       { type: "close" },
@@ -176,8 +178,78 @@ async function submit_attempt(body){
   });
   // const data2 = await response2.json();
   // console.log(data2.results[1]);
-  return score;
+  return { score: score, max_score: max_score };
+}
 
+async function get_user_quizes(body) {
+  const user_address = body.user_address;
+  const payload = {
+    requests: [
+      {
+        type: "execute",
+        stmt: {
+          sql: `SELECT * FROM quiz
+          WHERE owner = '${user_address}'`,
+        },
+      },
+      { type: "close" },
+    ],
+  };
+  const uri = "https://quiz3-notnotrachit.turso.io/v2/pipeline";
+  const response = await fetch(uri, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ` + TursoKEY,
+    },
+    body: JSON.stringify(payload),
+  });
+  const data = await response.json();
+  const quizes = [];
+  for (let i = 0; i < data.results[0].response.result.rows.length; i++) {
+    quizes.push({
+      quiz_name: data.results[0].response.result.rows[i][2].value,
+      quiz_description: data.results[0].response.result.rows[i][3].value,
+      quiz_id: data.results[0].response.result.rows[i][0].value,
+    });
+  }
+  return quizes;
+}
+
+async function get_leaderboard(body) {
+  // get the attempts ordered by score and timestamp
+  const quiz_id = body.quiz_id;
+  const payload = {
+    requests: [
+      {
+        type: "execute",
+        stmt: {
+          sql: `SELECT * FROM attempts WHERE quiz_id = '${quiz_id}' ORDER BY score DESC, timestamp ASC`,
+        },
+      },
+      { type: "close" },
+    ],
+  };
+  const uri = "https://quiz3-notnotrachit.turso.io/v2/pipeline";
+  const response = await fetch(uri, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ` + TursoKEY,
+    },
+    body: JSON.stringify(payload),
+  });
+  const data = await response.json();
+  const leaderboard = [];
+  for (let i = 0; i < data.results[0].response.result.rows.length; i++) {
+    leaderboard.push({
+      user_address: data.results[0].response.result.rows[i][2].value,
+      score: data.results[0].response.result.rows[i][4].value,
+      max_score: data.results[0].response.result.rows[i][5].value,
+      timestamp: data.results[0].response.result.rows[i][6].value,
+    });
+  }
+  return leaderboard;
 }
 
 export const main = async (params) => {
@@ -189,6 +261,10 @@ export const main = async (params) => {
       return get_quiz(body);
     case "/submit_attempt":
       return submit_attempt(body);
+    case "/get_user_quizes":
+      return get_user_quizes(body);
+    case "/get_leaderboard":
+      return get_leaderboard(body);
     default:
       return { error: "path not found" };
   }
